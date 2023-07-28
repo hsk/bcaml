9a6d88ef7500b912cf14bb9bb25342f6c2039747
wo4mei3 <wo4mei3@gmail.com>
2023-07-29 08:22:21 +0900

    make some progress

--- bin/defs.ml
+++ bin/defs.ml
@@ -1,10 +1,51 @@
 open Syntax
 open Typing
 
-let rec check_valid_fields tyl = function
-| [] -> true
-| (_,Tvar {contents=Unbound{id=id;level=_}})::rest ->
-  List.exists (occursin id) tyl && check_valid_fields tyl rest
-| _::rest ->
-  check_valid_fields tyl rest
+let rec check_valid_ty tyl = function
+| Tvar {contents=Unbound{id=id;level=_}} -> List.exists (occursin id) tyl
+| Tvar {contents=Linkto t} -> check_valid_ty tyl t
+| Tlist t -> check_valid_ty tyl t
+| Tref t -> check_valid_ty tyl t
+| Tarrow(arg,ret) -> check_valid_ty tyl arg && check_valid_ty tyl ret
+| Ttuple tyl' -> List.for_all (fun t->check_valid_ty tyl t) tyl'
+| Tconstr(_,tyl') -> List.for_all (fun t->check_valid_ty tyl t) tyl'
+| Trecord(_,fields) ->
+    check_valid_fields tyl fields
+| Tvariant(_,fields) -> 
+    check_valid_fields tyl fields
+| _ -> true
 
+and check_valid_fields tyl fields =
+  List.for_all (fun (_,t)-> check_valid_ty tyl t) fields
+
+let rec is_abbrev name = function
+| Dabbrev(name',_,_)::_ when name = name' ->
+  true
+| _::rest ->
+  is_abbrev name rest
+| [] ->
+  false
+
+let rec abbrev_found_in_ty decl seen = function
+| Tvar _ -> seen
+| Tlist t -> abbrev_found_in_ty decl seen t
+| Tref t ->  abbrev_found_in_ty decl seen t
+| Tarrow(arg,ret) -> abbrev_found_in_ty decl (abbrev_found_in_ty decl seen arg) ret
+| Ttuple tyl -> List.fold_left (abbrev_found_in_ty decl) seen tyl
+| Tconstr(name,tyl) when is_abbrev name decl -> name::List.fold_left (abbrev_found_in_ty decl) seen tyl
+| Trecord(_,fields) ->
+    List.fold_left (abbrev_found_in_ty decl) seen (List.map snd fields)
+| Tvariant(_,fields) -> 
+    List.fold_left (abbrev_found_in_ty decl) seen (List.map snd fields)
+| _ -> seen
+
+
+let check_recursive_abbrev lhs rhs decl = 
+  let rec aux lhs rhs = function
+  | Dabbrev(name,_,ty)::rest ->
+    aux (name::lhs) (abbrev_found_in_ty decl rhs ty) rest
+  | _::rest ->
+    aux lhs rhs rest
+  | [] ->
+    List.exists (fun e -> List.mem e rhs) lhs
+  in aux lhs rhs decl
