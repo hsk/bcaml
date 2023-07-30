e7dc624ef455329a98142f6c9ad4a72611ad697b
wo4mei3 <wo4mei3@gmail.com>
2023-07-30 09:24:43 +0900

    make some progress

--- bin/defs.ml
+++ bin/defs.ml
@@ -18,6 +18,23 @@ let rec check_valid_ty tyl = function
 and check_valid_fields tyl fields =
   List.for_all (fun (_,t)-> check_valid_ty tyl t) fields
 
+type status = 
+| Checking
+| Checked
+
+let name_is_checking name seen =
+  if List.mem_assoc name seen then
+    !(List.assoc name seen) = Checking
+  else 
+    false
+
+let name_is_checked name seen =
+  if List.mem_assoc name seen then
+    !(List.assoc name seen) = Checked
+  else 
+    false
+
+
 let rec is_abbrev name = function
 | Dabbrev(name',_,_)::_ when name = name' ->
   true
@@ -27,24 +44,41 @@ let rec is_abbrev name = function
   false
 
 let rec abbrev_found_in_ty decl seen = function
-| Tvar _ -> seen
 | Tlist t -> abbrev_found_in_ty decl seen t
 | Tref t ->  abbrev_found_in_ty decl seen t
-| Tarrow(arg,ret) -> abbrev_found_in_ty decl (abbrev_found_in_ty decl seen arg) ret
-| Ttuple tyl -> List.fold_left (abbrev_found_in_ty decl) seen tyl
-| Tconstr(name,tyl) when is_abbrev name decl -> name::List.fold_left (abbrev_found_in_ty decl) seen tyl
-| _ -> seen
+| Tarrow(arg,ret) -> abbrev_found_in_ty decl seen arg; abbrev_found_in_ty decl seen ret
+| Ttuple tyl -> List.iter (abbrev_found_in_ty decl seen) tyl
+| Tconstr(name,_) when name_is_checking name seen -> 
+  failwith "recursive type abbreviation"
+| Tconstr(name,tyl) when name_is_checked name seen -> 
+  List.iter (abbrev_found_in_ty decl seen) tyl
+| Tconstr(name,tyl) when is_abbrev name decl ->
+  abbrev_found_in_decl name seen decl;
+  List.iter (abbrev_found_in_ty decl seen) tyl
+| _ -> ()
 
-
-let check_recursive_abbrev lhs rhs decl = 
-  let rec aux lhs rhs = function
-  | Dabbrev(name,_,ty)::rest ->
-    aux (name::lhs) (abbrev_found_in_ty decl rhs ty) rest
+and abbrev_found_in_decl name seen decl = 
+  let rec aux = function
+  | Dabbrev(n,_,ty)::_ when n = name ->
+    let pair = (name,ref Checking) in
+    abbrev_found_in_ty decl (pair::seen) ty;
+    snd pair := Checked
   | _::rest ->
-    aux lhs rhs rest
+    aux rest
   | [] ->
-    List.for_all (fun e -> List.mem e rhs) lhs
-  in aux lhs rhs decl
+    failwith "name not found abbrev_found_in_decl"
+  in aux decl
+
+let check_recursive_abbrev decl = 
+  let rec aux = function 
+  | Dabbrev(name,_,_)::rest -> 
+    abbrev_found_in_decl name [] decl;
+    aux rest
+  | _::rest ->
+    aux rest
+  | [] ->
+    ()
+  in aux decl
 
 let rec is_def name = function
 | Drecord(name',_,_)::_ when name = name' ->
@@ -57,24 +91,51 @@ let rec is_def name = function
   false
 
 let rec def_found_in_ty decl seen = function
-| Tvar _ -> seen
 | Tlist t -> def_found_in_ty decl seen t
-| Tarrow(arg,ret) -> def_found_in_ty decl (def_found_in_ty decl seen arg) ret
-| Ttuple tyl -> List.fold_left (def_found_in_ty decl) seen tyl
+| Tarrow(arg,ret) -> def_found_in_ty decl seen arg; def_found_in_ty decl seen ret
+| Ttuple tyl -> List.iter (def_found_in_ty decl seen) tyl
+| Trecord(name,_) when name_is_checking name seen -> 
+  failwith "recursive type abbreviation"
+| Trecord(name,fields) when name_is_checked name seen -> 
+  List.iter (def_found_in_ty decl seen) (List.map snd fields)
 | Trecord(name,fields) when is_def name decl ->
-    name::List.fold_left (def_found_in_ty decl) seen (List.map snd fields)
-| Tvariant(name,fields) when is_def name decl -> 
-    name::List.fold_left (def_found_in_ty decl) seen (List.map snd fields)
-| _ -> seen
+  def_found_in_decl name seen decl;
+  List.iter (abbrev_found_in_ty decl seen) (List.map snd fields)
+| Tvariant(name,_) when name_is_checking name seen -> 
+  failwith "recursive type abbreviation"
+| Tvariant(name,fields) when name_is_checked name seen -> 
+  List.iter (def_found_in_ty decl seen) (List.map snd fields)
+| Tvariant(name,fields) when is_def name decl ->
+  def_found_in_decl name seen decl;
+  List.iter (abbrev_found_in_ty decl seen) (List.map snd fields)
+| _ -> ()
 
-let check_recursive_def lhs rhs decl = 
-  let rec aux lhs rhs = function
-  | Drecord(name,_,fields)::rest ->
-    aux (name::lhs) (List.fold_left (def_found_in_ty decl) rhs (List.map snd fields)) rest
-  | Dvariant(name,_,fields)::rest ->
-    aux (name::lhs) (List.fold_left (def_found_in_ty decl) rhs (List.map snd fields)) rest
+and def_found_in_decl name seen decl = 
+  let rec aux = function
+  | Drecord(n,_,fields)::_ when n = name ->
+    let pair = (name,ref Checking) in
+    List.iter (fun t->def_found_in_ty decl (pair::seen) (convert_constr t)) (List.map snd fields);
+    snd pair := Checked
+  | Dvariant(n,_,fields)::_ when n = name ->
+    let pair = (name,ref Checking) in
+    List.iter (fun t->def_found_in_ty decl (pair::seen) (convert_constr t)) (List.map snd fields);
+    snd pair := Checked
   | _::rest ->
-    aux lhs rhs rest
+    aux rest
   | [] ->
-    List.for_all (fun e -> List.mem e rhs) lhs
-  in aux lhs rhs decl
+    failwith "name not found def_found_in_decl"
+  in aux decl
+
+let check_recursive_def decl = 
+  let rec aux = function 
+  | Drecord(name,_,_)::rest -> 
+    def_found_in_decl name [] decl;
+    aux rest
+  | Dvariant(name,_,_)::rest -> 
+    def_found_in_decl name [] decl;
+    aux rest 
+  | _::rest ->
+    aux rest
+  | [] ->
+    ()
+  in aux decl
